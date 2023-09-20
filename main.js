480624ee2d4f7ac8e0166adc13eed7230b5e525a
RyanPintoPassos <119443198+RyanPintoPassos@users.noreply.github.com>
2023-09-20 14:54:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+object = [];
+status = "";
+
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function modelLoaded() {
+    
+}
